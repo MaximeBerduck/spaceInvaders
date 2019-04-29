d9eff07a3543f260174ed4ef376a1c75c8efb50c
Maxime Berduck <maxime.berduck@etu.unilim.Fr>
2019-04-29 16:27:29 +0200

    positionner un vaisseau avec une dimension

--- src/main/java/fr/unilim/fr/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/fr/spaceinvaders/SpaceInvaders.java
@@ -1,5 +1,6 @@
 package fr.unilim.fr.spaceinvaders;
 
+import fr.unilim.fr.spaceinvaders.utils.DebordementEspaceJeuException;
 import fr.unilim.fr.spaceinvaders.utils.HorsEspaceJeuException;
 
 public class SpaceInvaders {
@@ -60,10 +61,14 @@ public class SpaceInvaders {
 
 	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
 		if (!estDansEspaceJeu(x, y))
-		    throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+
+		if ( !estDansEspaceJeu(x+longueur-1,y))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers la droite à cause de sa longueur");
+		if (!estDansEspaceJeu(x,y-hauteur+1))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers le bas à cause de sa hauteur");
 
 		vaisseau = new Vaisseau(longueur, hauteur);
 		vaisseau.positionner(x, y);
-		
 	}
 }
